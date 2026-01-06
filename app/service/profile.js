627a271a7a4f6077a02355a88ce1bdc2e118899d
Dedede12345 <gleb@adenisov.com>
2026-01-06 14:30:28 +0100

    Added subscripitons sections

--- app/service/profile.js
+++ app/service/profile.js
@@ -1,21 +1,31 @@
 const Post = require("../database/models/Post");
 const User = require("../database/models/User");
-
-
-const env = require("dotenv");
-
-env.config();
+const Subscription = require("../database/models/Subscription");
 
 const profile = async (req, res) => {
     try {
         const userId = req.userId;
 
         const user = await User.findById(userId).lean();
+
         const posts = await Post.find({ author: userId })
             .sort({ postDate: -1 })
             .lean();
 
-        res.render("profile", { user, posts });
+        const subscribers = await Subscription.find({ publisher: userId })
+            .populate("subscriber", "username email")
+            .lean();
+
+        const subscriptions = await Subscription.find({ subscriber: userId })
+            .populate("publisher", "username email")
+            .lean();
+
+        res.render("profile", {
+            user,
+            posts,
+            subscribers,
+            subscriptions
+        });
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "ERROR OCCURRED" });
